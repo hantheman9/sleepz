70ddb204e34f984a8fda6bfdaa903e85ca4561ba
Burhan Draksibai <burhandraksibai@gmail.com>
2023-01-31 23:28:00 -0500

    Initial Commit

---
+++ sleepz Watch App/sleepzApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleepzApp.swift
+//  sleepz Watch App
+//
+//  Created by Burhan Drak Sibai on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct sleepz_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sleepz Watch AppUITests/sleepz_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sleepz_Watch_AppUITestsLaunchTests.swift
+//  sleepz Watch AppUITests
+//
+//  Created by Burhan Drak Sibai on 1/31/23.
+//
+
+import XCTest
+
+final class sleepz_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
